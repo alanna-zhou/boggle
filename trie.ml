66d6b991d56f5b4f73cc21493b033cdb358230ad
Austin Chen <alc265@cornell.edu>
2019-04-15 23:52:29 -0400

    Minor tweaks

--- trie.ml
+++ trie.ml
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 type t = Head of t list | Node of (string * t list * bool) | Leaf 
 
 let empty = Head ([Leaf])
@@ -31,6 +30,11 @@ let add_word trie word =
   | Head  (children) -> Head (update_children children word)
   | _ -> add_word_help trie word
 
+let rec add_words trie words =
+  match words with
+  | [] -> trie
+  | x::xs -> add_words (add_word trie x) xs
+
 let add_words_from_file (filename:string) : t =
   failwith "unimplemented"
 
@@ -45,7 +49,6 @@ let rec contains_help trie word =
 and check_char children word =
   if word = "" then true else
   let curchar = String.sub word 0 1 in 
-  let tail = String.sub word 1 ((String.length word) - 1) in 
   match children with
   | (Node (c, childs, is_word))::xs ->
   if c = curchar then contains_help (Node (c, childs, is_word)) word
@@ -59,48 +62,5 @@ let contains (trie:t) (word:string) : bool =
   | Head (children) -> check_char children word
   | _ -> contains_help trie word
   
-=======
-(* type t = Head of t list | Node of (string * t list) | Leaf  *)
-
-type t = string list
-
-(* let empty = Leaf  *)
-
-let empty = []
-
-(* let rec add_word trie word = 
-   failwith "unimplemented" *)
-(* match trie with
-   | Head  (children) -> begin
-    match children with
-    | x::xs -> begin
-        match x with
-        | Node (c, children) -> if c = (String.sub word 0 1) then
-            add_word x (String.sub word 1 (String.length word - 1))
-        | Leaf -> add_word x word
-      end
-   end
-   | Node (c, children) -> begin
-    match children with
-    | Leaf -> 
-
-   end *)
-
-let add_word (trie:t) (word:string) : t =
-  if List.mem word trie then trie else word::trie
-(* failwith "unimplemented" *)
-
-let rec add_words (trie:t) (words:string list) : t =
-  match words with 
-  | [] -> trie
-  | h::t -> add_words (add_word trie h) t 
-
-let add_words_from_file (filename:string) : t =
-  add_words empty ["RYE"; "TIE"; "DIE"]
-
-let contains (trie:t) (word:string) : bool =
-  List.mem word trie 
-
->>>>>>> acf2f8d21a276367dabb7d6b7e09a6346820560b
 let to_list (trie:t) : string list =
-  trie
+  failwith "unimplemented"
