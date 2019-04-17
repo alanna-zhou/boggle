e732685d10c5c3e8b6f15520c11551186658decf
James <jgd78@cornell.edu>
2019-04-16 21:03:36 -0400

    updated main

--- main.ml
+++ main.ml
@@ -10,53 +10,59 @@ let rec make_list lst acc=
   |[]-> acc
   |h::t-> make_list t (h ^ "; " ^ acc)
 
-let rec playing_game time st found_wrds =
-  if time<=Unix.time ()
+let rec start_game () =
+  print_endline "\n What kind of board would you like?";
+  print_string  "\n Type s for Standard and r for Random. :> "; 
+  match  read_line () with
+  |"s" -> playing_game (Unix.time() +. 90.) (State.init (Board.generate (Standard (4)))) []
+  |"r" -> playing_game (Unix.time() +. 90.) (State.init (Board.generate (Random(4)))) []
+  |_-> print_endline "\n Invalid entry"; start_game ()
+
+and playing_game time st found_wrds =
+  if is_game_over time
   then 
-    try print_string("Game Over \n Your score: " ^ 
-                     (string_of_int (State.score st)) ^ "\n Play again? y/n \n"); 
-      match read_line () with
-      |"y" -> start_game ()
-      |"n" -> ()
-      |_ -> raise (Failure "Invalid Input")
-    with 
-    |Failure x -> print_endline "This is not a valid input."; 
-      (playing_game time st found_wrds)
+    end_game  st
   else  
     try Board.format (State.board st) (Board.size (State.board st));
-      print_string ("Words found: " ^ (make_list found_wrds "") ^ "\n Enter a word:>");
+      print_string ("\n Words found: " ^ (make_list found_wrds "") ^ "\n Enter a word:>");
       match Command.parse(read_line ()) with
       |Quit -> playing_game (Unix.time ()) st found_wrds
-      |Score -> print_string ("Your score: " ^ string_of_int (State.score st));
+      |Score -> print_string ("\n Your score: " ^ string_of_int (State.score st));
         playing_game time st found_wrds
-      |Help -> print_string "To enter a word, enter that word.\n
+      |Help -> print_string "\n To enter a word, enter that word.\n
                           To see your current score, enter #score.\n
                           To quit/restart game, enter #quit.\n
                           To see instructions, enter #help.";
         playing_game time st found_wrds
       |Entry (guess) -> 
-        if (List.mem guess found_wrds)
-        then playing_game time st found_wrds
-        else (playing_game time (State.update st guess) (guess :: found_wrds))
+        if is_game_over time then end_game st 
+        else if List.mem guess found_wrds then playing_game time st found_wrds
+        else if not (Board.is_valid_word guess (State.board st)) 
+        then raise (Failure guess)
+        else playing_game time (State.update st guess) (guess :: found_wrds)
     with 
-    |Failure x -> print_endline "This is not a valid input."; 
+    |Failure x -> print_endline (x ^ "is not a valid input."); 
       playing_game time st found_wrds
-    |Empty -> print_string "Entry is empty, choose another word.";
+    |Empty -> print_string "\n Entry is empty, choose another word.";
       playing_game time st found_wrds
+      
+and end_game st =
+  print_string("\n Game Over \n Your score: " ^ (string_of_int (State.score st))); 
+  prompt_end ()
 
-and  start_game () =
-  print_endline "What kind of board would you like?";
-  print_string  "Type s for Standard and r for Random> "; 
-  match  read_line () with
-  |"s" -> playing_game (Unix.time() +. 90.) (State.init (Board.generate (Standard (4)))) []
-  |"r" -> playing_game (Unix.time() +. 90.) (State.init (Board.generate (Random(4)))) []
-  |_-> print_endline "Invalid entry"; start_game ()
-
-
+and prompt_end () =
+  print_string "\n Play again? y/n :>";
+  match read_line () with 
+  |"y" -> start_game ()
+  |"n" -> ()
+  |_ -> print_string "Not a valid input."; prompt_end ()
+and is_game_over time =
+  Unix.time () > time
 
 
 let main () =
-  print_string "Welcome to Boggle.\n";
+  print_string "Welcome to Word Blitz! At any time, type #help for the gameplay
+   instructions. \n";
   start_game ()
 
 let () = main()
