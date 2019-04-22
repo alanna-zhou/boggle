778332a93467213651832f871acb588d29afb260
James <jgd78@cornell.edu>
2019-04-22 00:06:50 -0400

    updated main

--- main.ml
+++ main.ml
@@ -5,20 +5,40 @@ open State
 type game = {
   state : State.t;
 }
+let clear = function x -> Sys.command("clear")+x
 let rec make_list lst acc=
   match lst with 
   |[]-> acc
   |h::t-> make_list t (h ^ "; " ^ acc)
 
-let rec start_game () =
+let rec prompt_board_size () =
+  try 
+    let size = int_of_string (read_line()) in size
+  with 
+  |Failure x-> ANSITerminal.(print_string [red] "Invalid size, try again.");
+    prompt_board_size ()
+
+and prompt_board_type () =
+  let s = prompt_board_size () in
   print_endline "\nWhat kind of board would you like?";
-  print_string  "\nType s for Standard and r for Random. : "; 
+  print_string  "\nType s for Standard, r for Random, or c for Custom. "; 
   match  read_line () with
-  |"s" -> playing_game (Unix.time() +. 90.) 
-            (State.init (Board.generate (Standard (4)))) []
-  |"r" -> playing_game (Unix.time() +. 90.) 
-            (State.init (Board.generate (Random(4)))) []
-  |_-> print_endline "\nInvalid entry"; start_game ()
+  |"s" -> print_string "Would you like the board to be a 4x4 or 5x5?";
+    if s =4 || s = 5 then
+      playing_game (Unix.time() +. 90.) 
+        (State.init (Board.generate (Standard (s)))) []
+    else ANSITerminal.(print_string [red] "\nInvalid entry";
+                       prompt_board_type ());
+  |"r" -> 
+    if s < 31 
+    then playing_game (Unix.time() +. 90.) 
+        (State.init (Board.generate (Random (s)))) []
+    else print_string "\nRandom size must be less than or equal to 30."; 
+    prompt_board_type ()
+  |"c" -> playing_game (Unix.time() +. 90.) 
+            (State.init (Board.generate (Custom))) []
+  |_-> ANSITerminal.(print_string [red] "\nInvalid entry"; prompt_board_type ());
+
 and playing_game time st found_wrds =
   if is_game_over time
   then 
@@ -39,35 +59,44 @@ To quit/restart game, enter #quit.
 To see instructions, enter #help.";
         playing_game time st found_wrds
       |Entry (guess) -> 
+        ignore(clear 0);
         if is_game_over time then end_game st 
         else if List.mem guess found_wrds then playing_game time st found_wrds
         else if not (Board.is_valid_word guess (State.board st)) 
         then raise (Failure guess)
         else playing_game time (State.update st guess) (guess :: found_wrds)
     with 
-    |Failure x -> print_endline (x ^ " is not a valid input."); 
+    |Failure x -> ignore(clear 0);
+      ANSITerminal.(print_string [red] (x));
+      print_string (" is not a valid input."); 
       playing_game time st found_wrds
-    |Empty -> print_string "\nEntry is empty, choose another word.";
+    |Empty -> ignore(clear 0); 
+      ANSITerminal.(print_string [red] "\nEntry is empty, choose another word.");
       playing_game time st found_wrds
 
 and end_game st =
-  print_string("\nGame Over \nYour score: " ^ (string_of_int (State.score st))); 
+  ANSITerminal.(print_string [red] "\nGame Over)); 
+  print_string (\nYour score: ");
+  ANSITerminal.(print_string [green](string_of_int (State.score st))); 
   prompt_end ()
 
 and prompt_end () =
   print_string "\nPlay again? y/n : ";
   match read_line () with 
-  |"y" -> start_game ()
+  |"y" -> prompt_board_type ()
   |"n" -> ()
-  |_ -> print_string "Not a valid input."; prompt_end ()
+  |_ -> ANSITerminal.(print_string [red]"Not a valid input."); prompt_end ()
+
 and is_game_over time =
   Unix.time () >= time
 
 
 let main () =
-  print_string "Welcome to Word Blitz! At any time, type #help for the gameplay
-   instructions. \n";
-  start_game ()
+  print_string "Welcome to Word Blitz! Form and enter words contained on the
+   board by connecting letters horizontally, vertically, or diagonally.
+    At any time, type #help for 
+   gameplay instructions. \n";
+  prompt_board_type ()
 
 let () = main()
 
