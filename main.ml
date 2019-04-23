e88ee3e7d695432318cce0a567210a16e1d5100a
Khyati Sipani <ks965@cornell.edu>
2019-04-22 20:33:12 -0400

    leaderboard working

--- main.ml
+++ main.ml
@@ -14,59 +14,73 @@ let rec make_list lst acc =
   |h::t-> make_list t (h ^ "; " ^ acc)
 
 (** [start_game] starts the game and allows for user input.  *)
-let rec start_game () =
+let rec start_game game_number leaderboard () =
   print_endline "\nWhat kind of board would you like?";
   print_string  "\nType s for Standard and r for Random. : "; 
-  match  read_line () with
-  |"s" -> playing_game (Unix.time() +. 90.) 
-            (State.init (Board.generate (Standard (4)))) []
-  |"r" -> playing_game (Unix.time() +. 90.) 
-            (State.init (Board.generate (Random(4)))) []
-  |_-> print_endline "\nInvalid entry"; start_game ()
-and playing_game time st found_wrds =
-  let () = Random.self_init () in 
-  if is_game_over time
-  then 
-    end_game  st
-  else  
+  if game_number = 0 then begin 
+    match  read_line () with
+    |"s" -> playing_game (Unix.time() +. 90.) 
+              (State.init (Board.generate (Standard (4))) []) [] game_number
+    |"r" -> playing_game (Unix.time() +. 90.) 
+              (State.init (Board.generate (Random(4))) [])  [] game_number
+    |_-> print_endline "\nInvalid entry"; start_game game_number leaderboard ()
+  end else begin 
+    match read_line () with 
+    |"s" -> playing_game (Unix.time() +. 90.) 
+              (State.init (Board.generate (Standard (4))) leaderboard) [] game_number
+    |"r" -> playing_game (Unix.time() +. 90.) 
+              (State.init (Board.generate (Random(4))) leaderboard) [] game_number
+    |_-> print_endline "\nInvalid entry"; start_game game_number leaderboard ()
+  end
+and playing_game time st found_wrds game_number =
+  if is_game_over time then 
+    end_game game_number st 
+  else begin 
     try 
       print_string "\n"; 
       Board.format (State.board st) (Board.size (State.board st));
       print_string ("\nWords found: " ^ 
                     (make_list found_wrds "") ^ "\nEnter a word: ");
-      match Command.parse(read_line ()) with
-      |Quit -> end_game st
+      match (Command.parse(read_line ())) with
+      (*|Quit -> print_string "hi"; end_game game_number st*)
       |Score -> print_string ("\nYour score: " ^ string_of_int (State.score st));
-        playing_game time st found_wrds
+        playing_game time st found_wrds game_number
+      |Quit -> end_game game_number st 
       |Help -> print_string "\nTo enter a word, enter that word.
 To see your current score, enter #score.
 To quit/restart game, enter #quit.
 To see instructions, enter #help.";
-        playing_game time st found_wrds
+        playing_game time st found_wrds game_number
       |Entry (guess) -> 
-        if is_game_over time then end_game st 
-        else if List.mem guess found_wrds then playing_game time st found_wrds
+        if is_game_over time then end_game game_number st 
+        else if List.mem guess found_wrds then playing_game time st found_wrds game_number
         else if not (Board.is_valid_word guess (State.board st)) 
         then raise (Failure guess)
-        else playing_game time (State.update st guess) (guess :: found_wrds)
+        else begin
+          let new_state = State.update st guess in 
+          playing_game time (new_state) (guess :: found_wrds) game_number
+        end
     with 
     |Failure x -> print_endline (x ^ " is not a valid input."); 
-      playing_game time st found_wrds
+      playing_game time st found_wrds game_number
     |Empty -> print_string "\nEntry is empty, choose another word.";
-      playing_game time st found_wrds
+      playing_game time st found_wrds game_number
+  end 
 
 (** [end_game] ends the game.  *)
-and end_game st =
-  print_string("\nGame Over \nYour score: " ^ (string_of_int (State.score st))); 
-  prompt_end ()
+and end_game game_number st =
+  print_string("\nGame Over \nYour score: " ^ (string_of_int (State.score st)));
+  let new_leaderboard = add_leaderboard (leaderboard st) ([score st]) (size (board st)) []  in
+  let () = print_leaderboard new_leaderboard in 
+  (prompt_end game_number (new_leaderboard) ())
 
 (** [prompt_end] asks for user input on whether or not they'd like to continue playing.  *)
-and prompt_end () =
+and prompt_end game_number leaderboard () =
   print_string "\nPlay again? y/n : ";
   match read_line () with 
-  |"y" -> start_game ()
-  |"n" -> ()
-  |_ -> print_string "Not a valid input."; prompt_end ()
+  |"y" -> start_game (game_number + 1) (leaderboard) () 
+  |"n" -> () 
+  |_ -> print_string "Not a valid input."; prompt_end game_number leaderboard ()
 and is_game_over time =
   Unix.time () >= time
 
@@ -74,7 +88,7 @@ and is_game_over time =
 let main () =
   print_string "Welcome to Word Blitz! At any time, type #help for the gameplay
    instructions. \n";
-  start_game ()
+  start_game 0 [] ()
 
 let () = main ()
 
--- state.ml
+++ state.ml
@@ -7,27 +7,73 @@ type t = {
   board : Board.t;
   score : int;
   words : Trie.t;
+  leaderboard: (int * (int list)) list 
 }
 
 (** [init] initializes a state given a board. *)
-let init (board:Board.t) : t = 
-  {board=board;score=0;words=Trie.empty}
+let init (board:Board.t) (leaderboard): t = 
+  {board=board;score=0;words=Trie.empty;leaderboard = leaderboard}
 
-(** [score] provides the score of the list of words that the player has been entering to play the board in the state.  *)
+(** [score] provides the score of the list of words that the player has been 
+    entering to play the board in the state.  *)
 let score (state:t) : int = 
   state.score
 
-(** [board] provides the board that the state has been keeping track for the player.  *)
+(** [board] provides the board that the state has been keeping track for the 
+    player.  *)
 let board (state:t) : Board.t = 
   state.board
 
-(** [words] provides the list of words that the player has been entering to play the game.  *)
+(** [words] provides the list of words that the player has been entering to 
+    play the game.  *)
 let words (state:t) : string list =
   Trie.to_list state.words
 
-(** [update] updates a state with a newly added word by validating if the word is a valid word (on the board and in the English language - this is dependent upon Board's implementation of [is_valid_word]). *)
+let leaderboard (state:t) = 
+  state.leaderboard
+
+(** [update] updates a state with a newly added word by validating if the word 
+    is a valid word (on the board and in the English language - this is dependent 
+    upon Board's implementation of [is_valid_word]). *)
 let update (state:t) (word:string) : t =
   let new_words = Trie.add_word state.words word in 
   let word_score = Board.word_score word state.board in 
   let new_score = state.score + word_score in 
-  {board=state.board;score=new_score;words=new_words}
+  {board=state.board;score=new_score;words=new_words;
+   leaderboard=(leaderboard state)}
+
+let key_compare x y = 
+  if fst x < fst y then -1
+  else if fst x > fst y then 1
+  else 0 
+
+let rec print_score_list = function
+  | [] -> ()
+  | h::t -> print_int h; print_string "\n"; print_score_list t
+
+let rec print_leaderboard_helper l = 
+  match l with
+  | [] -> ()
+  | (size, score_lst)::t -> let str_size = string_of_int size in 
+    let size_string = "\n"^str_size^"x"^str_size^":\n" in 
+    ANSITerminal.(print_string [green] size_string); 
+    print_score_list score_lst; 
+    print_leaderboard_helper t 
+
+let print_leaderboard l = print_string "\n"; 
+  if l = [] then () else 
+    (ANSITerminal.(print_string [red] "******LEADERBOARD********"); 
+     print_leaderboard_helper (List.rev (List.sort key_compare l)))
+
+let rec add_leaderboard (old_leaderboard) (score:int list) (size:int) (acc) =  
+  match old_leaderboard with 
+  | [] -> ((size, score)::acc)
+  | (h_size, lst)::t -> if h_size = size then begin
+      let new_lst = (score@lst) in 
+      let sorted_list = List.rev (List.sort compare new_lst) in 
+      add_leaderboard t sorted_list size (acc) 
+    end else add_leaderboard t score size ((h_size, lst)::acc)
+
+
+
+
